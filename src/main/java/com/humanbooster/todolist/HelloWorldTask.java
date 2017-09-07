5aa8c16da74006936e1d9efbd9b24c8ce595f147
felix-heintzen <felix.heintzen@orange.fr>
2017-09-07 15:57:05 +0200

    suppr variable JulienAdoreLinux

--- src/main/java/com/humanbooster/todolist/HelloWorldTask.java
+++ src/main/java/com/humanbooster/todolist/HelloWorldTask.java
@@ -10,4 +10,5 @@ public class HelloWorldTask {
     public String dateFin;
     public int tachePere = -1; // Permet de connaitre le père sans faire de scan
     public List<Integer> tacheEnfants = new ArrayList<Integer>(); // Permet de connaitre les enfants sans scan
+    public String JulienAdoreLinux;
 }
